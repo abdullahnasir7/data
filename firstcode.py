58bf9cd05f70c3437afa3f14a6fe9222a945972f
Muhammad Abdullah <mabdullah7500941@gmail.com>
2026-06-18 17:20:55 +0500

    i create a functio for cold_drink

--- firstcode.py
+++ firstcode.py
@@ -1,1 +1,8 @@
 print("hello world!")
+def cold_drink():
+    option = "coca cola"
+    if option == "coca cola":
+        print("Thanks ! You are drinking coca cola.")
+    else:
+        print("Thanks! you are not drinking coca cola")
+cold_drink()
